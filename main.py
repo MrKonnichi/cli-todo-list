b420541ac986dd8ae7ca39ee93acccd2c97f91d4
MrKonnichi <auckme@proton.me>
2025-12-05 14:45:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import os
 import sys
 
-def add(text):
+def clear(): #clears the terminal
+	if os.name == "nt":
+		os.system("cls")
+	if os.name == "posix":
+		os.system("clear")
+
+def add(text): #adds a task (add <text of task>)
 	with open("todo.txt","a") as file:
 		file.write(text+"\n")
 		
-def remove(task):
+def remove(task): #removes a task (remove <number of task>)
 	with open("todo.txt","r") as file:
 		tasks=file.readlines()
 		tasks.pop(task)
 	with open("todo.txt","w") as file:
 		file.writelines(tasks)
 		
-def removeall():
+def removeall(): #removes all tasks (remove all)
 	with open("todo.txt","w") as file:
 		file.write("")
 		
-def mark(number):
+def mark(number): #marks a task (mark <number of task>)
 	with open("todo.txt","r") as file:
 		tasks=file.readlines()
 		if tasks[number].startswith("[MARKED]"):
@@ -27,7 +33,7 @@ def mark(number):
 	with open("todo.txt","w") as file:
 		file.writelines(tasks)
 		
-def unmark(number):
+def unmark(number): #unmarks a task (unmark <numbrer of task>)
 	with open("todo.txt","r") as file:
 		tasks=file.readlines()
 		if tasks[number].startswith("[MARKED]"):
@@ -38,35 +44,35 @@ def unmark(number):
 				file.writelines(tasks)
 			
 		
-def main():
+def main(): #the main fuction
 	with open("todo.txt","r") as file:
-		lines = file.readlines()
-	os.system("clear")
-	if not lines:
+		tasks = file.readlines()
+	clear() #clears previous frames
+	if not tasks: #if no tasks exist
 		commands=input("")
 		if commands.split()[0] == "add":
 			try:
 				add(commands.split()[1])
 			except IndexError:
-				print("please use the following syntax: add <text>")
-	else:
+				pass
+	else: #if tasks do exist
 		with open("todo.txt","r") as file:
-			lines = file.readlines()
-		for number, task in enumerate(lines):
+			tasks = file.readlines()
+		for number, task in enumerate(tasks): #printing tasks
 			if task.startswith("[MARKED]"):		
 				print(f"{number+1}. {task[8:].rstrip()} ☑\n")
 			else:
 				print(f"{number+1}. {task}")
 		commands=input("""
-""")
-		if commands:
+""") #user input
+		if commands: #if input exists process it
 			match commands.split()[0]:
-				case "add":
+				case "add": #adding a task
 					try:
 						add(commands[4:])
 					except (IndexError,ValueError):
 						pass
-				case "remove":
+				case "remove": #removing a task/all tasks
 					try:
 						if commands.split()[1] == "all":
 							removeall()
@@ -74,20 +80,20 @@ def main():
 							remove(int(commands.split()[1])-1)
 					except (IndexError,ValueError):
 						pass
-				case "mark":
+				case "mark": #marking a task
 					try:
 						if int(commands.split()[1]) > 0:
 							mark(int(commands.split()[1])-1)
 					except (IndexError,ValueError):
 						pass
-				case "unmark":
+				case "unmark": #unmarking a task
 					try:
 						if int(commands.split()[1]) > 0:
 							unmark(int(commands.split()[1])-1)
 					except (IndexError,ValueError):
 						pass
-				case "quit"|"exit":
-					os.system("clear")
+				case "quit"|"exit"|"close"|"leave": #closing the program
+					clear()
 					sys.exit()
 
 while True:
